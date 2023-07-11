419426364fd0a56e6af409ecca1680de649090d1
AbdurashiDIDO <abdurashiddido@gmail.com>
2023-07-11 22:10:30 +0500

    add docker-compose.yml

--- root/settings.py
+++ root/settings.py
@@ -48,8 +48,7 @@ ROOT_URLCONF = 'root.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
